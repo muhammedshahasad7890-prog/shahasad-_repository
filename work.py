791c87533e53f05b73b23abe558cc7e42fdcc839
shahasad <muhammedshahasad7890@gmail.com>
2025-12-10 12:20:59 +0530

    msg

--- work.py
+++ work.py
@@ -6,3 +6,21 @@ def add(x,y):
 
 def sub(x,y):
     return x-y
+
+
+
+def add(x,y):
+
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+    
+
+def add(x,y):
+
+    return x+y
+
+def sub(x,y):
+    return x-y
